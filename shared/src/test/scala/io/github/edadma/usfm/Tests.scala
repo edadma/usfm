0fe0f67fd265c327a6d37a8264798116907e7420
Edward A Maxedon Sr <edadma@gmail.com>
2024-10-18 11:37:35 -0400

    tests

--- shared/src/test/scala/io/github/edadma/usfm/Tests.scala
+++ shared/src/test/scala/io/github/edadma/usfm/Tests.scala
@@ -3,10 +3,20 @@ package io.github.edadma.usfm
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
-class Tests extends AnyFreeSpec with Matchers {
+class Tests extends AnyFreeSpec with Matchers:
 
-  "test" in {
-    tokenize("asdf") shouldBe Seq(Text())
+  "text 1" in {
+    tokenize("asdf") shouldBe Seq(Text("asdf"))
   }
 
-}
+  "text 2" in {
+    tokenize("asdf zxcv") shouldBe Seq(Text("asdf"), Space, Text("zxcv"))
+  }
+
+  "text 3" in {
+    tokenize("asdf  zxcv") shouldBe Seq(Text("asdf"), Space, Text("zxcv"))
+  }
+
+  "text 4" in {
+    tokenize("asdf\nzxcv") shouldBe Seq(Text("asdf"), Space, Text("zxcv"))
+  }
